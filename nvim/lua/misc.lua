ff26a0142de373bfacb737bcb079c78b3df6cde5
Assaf Dori <assafdori@Assafs-MacBook-Pro.local>
2024-04-28 20:27:42 +0300

    adding neogit keymap

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -25,5 +25,6 @@ vim.keymap.set("n", "<leader>ee", "<cmd>GoIfErr<cr>",
 )
 
 -- Git
+vim.api.nvim_set_keymap("n", "<leader>ng", ":Neogit<CR>", {noremap=false})
 vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit -m \"", {noremap=false})
 vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push -u origin HEAD<CR>", {noremap=false})
