5c6bddc8ba875e6fdb78721bf19c6a9f36d2765c
assafdori <assafxdori@gmail.com>
2024-07-22 12:30:03 +0300

    lazy.lua/NvimTreeRefresh

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -79,7 +79,7 @@ require('lazy').setup({
          vim.api.nvim_set_keymap("n", "ff", ":NvimTreeToggle<enter>", { noremap=false }),
          -- vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) 
          -- vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) 
-         vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+         -- vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
        })
      end,
    },
