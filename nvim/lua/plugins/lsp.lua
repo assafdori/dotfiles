03fc3caad7f504354b0ebe2816db9ca460f564bd
assafdori <assafxdori@gmail.com>
2024-07-27 16:05:53 +0300

    fix/lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -57,7 +57,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls', 'terraformls', 'lua_ls', 'yamlls', 'dockerls', 'groovy' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'gopls', 'terraformls', 'lua_ls', 'yamlls', 'dockerls', 'groovyls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
