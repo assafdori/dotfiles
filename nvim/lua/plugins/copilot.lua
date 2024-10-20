c59a66dd947cff040fbf33235eb2dc8668b2b425
Assaf Dori <assafdori@icloud.com>
2024-10-20 23:00:39 +0300

    nvim/copilot enable yaml file type

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -34,6 +34,9 @@ return {
                         dismiss = "<C-e>",
                     },
                 },
+                filetypes = {
+                    yaml = true,
+                }
             })
         end,
     },
