de012ab29993d7bcbd7abb7bbb960282f2e74f7d
Assaf Dori <assafdori@icloud.com>
2025-01-30 02:30:58 +0200

    [+] nvim ; neoscroll.nvim

--- nvim/lua/vnext/plugins/navigator.lua
+++ nvim/lua/vnext/plugins/navigator.lua
@@ -7,4 +7,15 @@ return {
     { "<C-l>", "<cmd>lua require('Navigator').right()<CR>" },
     { "<C-j>", "<cmd>lua require('Navigator').down()<CR>" },
   },
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({
+        stop_eof = true,
+        easing_function = "sine",
+        hide_cursor = true,
+        cursor_scrolls_alone = true,
+      })
+    end,
+  },
 }
