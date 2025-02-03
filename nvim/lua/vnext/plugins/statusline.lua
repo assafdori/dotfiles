fc28d4fb6d2581b12a611e58658d8c19c651fc1c
Assaf Dori <assafdori@icloud.com>
2025-02-03 17:15:59 +0200

    [~] nvim ; statusline

--- nvim/lua/vnext/plugins/statusline.lua
+++ nvim/lua/vnext/plugins/statusline.lua
@@ -27,7 +27,7 @@ return {
       },
       lualine_x = { "filetype" },
       lualine_y = { "progress" },
-      lualine_z = { "location" },
+      lualine_z = { "mode" },
     },
   },
   config = function(_, opts)
