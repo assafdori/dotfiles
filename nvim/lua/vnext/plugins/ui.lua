00eed30cb9daa3ea1b6dc2f41c1525d850bf69c8
Assaf Dori <assafdori@icloud.com>
2025-02-13 20:46:23 +0200

    [~] nvim ; noice
    command palette style

--- nvim/lua/vnext/plugins/ui.lua
+++ nvim/lua/vnext/plugins/ui.lua
@@ -55,7 +55,7 @@ return {
     },
     opts = {
       cmdline = {
-        view = "cmdline", -- classic cmdline at the botton
+        -- view = "cmdline", -- classic cmdline at the botton
       },
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -66,6 +66,7 @@ return {
         },
       },
       presets = {
+        command_palette = true,
         bottom_search = true, -- use a classic bottom cmdline for search
         long_message_to_split = true, -- long messages will be sent to a split
         lsp_doc_border = true, -- add a border to hover docs and signature help
