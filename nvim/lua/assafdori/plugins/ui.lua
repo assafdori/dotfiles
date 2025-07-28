7258c564ee3d24e93575f7e204a792865c5c1cce
Assaf Dori <assafdori@icloud.com>
2025-07-28 11:04:20 +0300

    feat(ui): ✨ add visual whitespace highlighting in visual mode

--- nvim/lua/assafdori/plugins/ui.lua
+++ nvim/lua/assafdori/plugins/ui.lua
@@ -136,4 +136,10 @@ return {
       vim.api.nvim_create_user_command("ToggleTheme", ToggleTheme, {})
     end,
   },
+  {
+    "mcauley-penney/visual-whitespace.nvim",
+    config = true,
+    event = "ModeChanged *:[vV\22]", -- optionally, lazy load on entering visual mode
+    opts = {},
+  },
 }
