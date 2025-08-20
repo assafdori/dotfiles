65043888052a1aeab52cc44aabf1da257e028acc
Assaf Dori <assafdori@icloud.com>
2025-08-20 13:41:15 +0300

    style: 💅 lualine refactor

--- nvim/lua/assafdori/plugins/statusline.lua
+++ nvim/lua/assafdori/plugins/statusline.lua
@@ -1,107 +1,80 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = "BufReadPost",
-  opts = {
-    extensions = { "fzf", "lazy", "mason", "quickfix", "neo-tree" },
-    options = {
-      disabled_filetypes = { "neo-tree", "snacks_dashboard", "Outline" },
-    },
-    sections = {
-      lualine_a = {}, -- hide mode
-      lualine_b = {
-        "branch",
-        "diff",
-        "diagnostics",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+  config = function()
+    -- Custom Lualine component to show attached language server
+    local clients_lsp = function()
+      local clients = vim.lsp.get_clients()
+      if next(clients) == nil then
+        return ""
+      end
+
+      local c = {}
+      for _, client in pairs(clients) do
+        table.insert(c, client.name)
+      end
+      return " " .. table.concat(c, "|")
+    end
+
+    require("lualine").setup({
+      options = {
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = { "alpha", "Outline", "snacks_dashboard" },
       },
-      lualine_c = {
-        {
-          "filename",
-          file_status = true, -- Displays file status (readonly status, modified status)
-          newfile_status = true, -- Display new file status (new file means no write after created)
-          path = 3, -- 0: Just the filename
-          -- 1: Relative path
-          -- 2: Absolute path
-          -- 3: Absolute path, with tilde as the home directory
-          -- 4: Filename and parent dir, with tilde as the home directory
+      sections = {
+        lualine_a = {
+          { "mode", icon = "" },
+        },
+        lualine_b = {
+          {
+            "filetype",
+            icon_only = true,
+            padding = { left = 1, right = 0 },
+          },
+          {
+            "filename",
+            path = 0, -- 0: Just the filename, 1: Relative path, 2: Absolute path
+            symbols = { modified = " ", readonly = " " },
+          },
+        },
+        lualine_c = {
+          {
+            "branch",
+            icon = "",
+          },
+          {
+            "diff",
+            symbols = { added = " ", modified = " ", removed = " " },
+            colored = true,
+          },
+        },
+        lualine_x = {
+          {
+            "diagnostics",
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+            update_in_insert = true,
+          },
+        },
+        lualine_y = { clients_lsp },
+        lualine_z = {
+          { "location", icon = "" },
         },
       },
-      lualine_x = { "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "mode" },
-    },
-  },
-  config = function(_, opts)
-    -- Show info when recording a macro
-    local function is_macro_recording()
-      local reg = vim.fn.reg_recording()
-      if reg == "" then
-        return ""
-      end
-      return "󰑋 " .. reg
-    end
-
-    table.insert(opts.sections.lualine_x, 1, {
-      is_macro_recording,
-      color = { fg = "#333333", bg = "#ff6666" },
-      separator = { left = "", right = "" },
-      cond = function()
-        return is_macro_recording() ~= ""
-      end,
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      extensions = { "toggleterm", "trouble" },
     })
-
-    -- Don't display encoding if encoding is UTF-8
-    local function encoding()
-      local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
-      return ret
-    end
-
-    table.insert(opts.sections.lualine_x, 1, {
-      encoding,
-      cond = function()
-        return encoding() ~= ""
-      end,
-    })
-
-    -- Don't display fileformat if fileformat is unix
-    local function fileformat()
-      local ret, _ = vim.bo.fileformat:gsub("^unix$", "")
-      return ret
-    end
-
-    table.insert(opts.sections.lualine_x, 1, {
-      fileformat,
-      cond = function()
-        return fileformat() ~= ""
-      end,
-    })
-
-    local function wordCount()
-      local wc = vim.fn.wordcount()
-      if wc == nil then
-        return ""
-      end
-      if wc["visual_words"] then -- text is selected in visual mode
-        return wc["visual_words"] .. " Words/" .. wc["visual_chars"] .. " Chars (Vis)"
-      else -- all of the document
-        return wc["words"] .. " Words"
-      end
-    end
-
-    table.insert(opts.sections.lualine_y, 1, {
-      wordCount,
-      cond = function()
-        local ft = vim.bo.filetype
-        local count = {
-          latex = true,
-          tex = true,
-          text = true,
-          markdown = true,
-          vimwiki = true,
-        }
-        return count[ft] ~= nil
-      end,
-    })
-
-    require("lualine").setup(opts)
+    vim.api.nvim_set_hl(0, "lualine_a_visual", { fg = "#000000", bg = "#dc4c44", bold = true })
+    vim.api.nvim_set_hl(0, "lualine_a_replace", { fg = "#000000", bg = "#dc4c44", bold = true })
+    vim.api.nvim_set_hl(0, "lualine_a_command", { fg = "#000000", bg = "#f9e2af", bold = true })
+    vim.api.nvim_set_hl(0, "lualine_a_insert", { fg = "#000000", bg = "#a6e3a1", bold = true })
   end,
 }
