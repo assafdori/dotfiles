0f38173aaa99333c5be5fc5f3b5685a4070d34ef
Assaf Dori <assafdori@icloud.com>
2025-09-06 13:10:35 +0300

    chore(plugins): relocate ufo.nvim to coding plugins

--- nvim/lua/assafdori/plugins/coding.lua
+++ nvim/lua/assafdori/plugins/coding.lua
@@ -95,4 +95,20 @@ return {
       require("lensline").setup()
     end,
   },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      require("ufo").setup({
+        open_fold_hl_timeout = 0, -- Disable automatic fold highlighting
+        provider_selector = function(_, _, _)
+          return { "treesitter", "indent" } -- Use Treesitter and indent as fallback
+        end,
+      })
+      vim.o.foldlevel = 99 -- Ensure folds are open by default
+      vim.o.foldlevelstart = 99 -- Start with all folds open
+      vim.o.foldenable = true -- Enable folding
+      -- vim.o.foldcolumn = "1" -- Show fold column
+    end,
+  },
 }
