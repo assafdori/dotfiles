78679d6005711047c98543e72f123edc45adf01c
Assaf Dori <assafdori@icloud.com>
2026-03-11 17:20:27 +0200

    fix: neoscroll smoother action

--- nvim/lua/assafdori/plugins/navigator.lua
+++ nvim/lua/assafdori/plugins/navigator.lua
@@ -19,12 +19,7 @@ return {
   {
     "karb94/neoscroll.nvim",
     config = function()
-      require("neoscroll").setup({
-        stop_eof = true,
-        easing_function = "sine",
-        hide_cursor = true,
-        cursor_scrolls_alone = true,
-      })
+      require("neoscroll").setup({})
     end,
   },
 }
