567e3e668834bbf7c4f03e2bee6d85190b0b32c4
Assaf Dori <assafdori@icloud.com>
2026-03-11 16:48:10 +0200

    feat: folke.flash

--- nvim/lua/assafdori/plugins/editing.lua
+++ nvim/lua/assafdori/plugins/editing.lua
@@ -50,34 +50,15 @@ return {
 
   {
     "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
     opts = {},
     keys = {
-      {
-        "ss",
-        mode = { "n", "x", "o" },
-        -- Jump to any word
-        function()
-          ---@diagnostic disable: missing-fields
-          require("flash").jump({
-            pattern = ".", -- initialize pattern with any char
-            search = {
-              mode = function(pattern)
-                -- remove leading dot
-                if pattern:sub(1, 1) == "." then
-                  pattern = pattern:sub(2)
-                end
-                -- return word pattern and proper skip pattern
-                return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
-              end,
-            },
-          })
-        end,
-        desc = "Flash",
-      },
-      -- stylua: ignore start
-      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter", },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash", },
-      -- stylua: ignore end
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
 
